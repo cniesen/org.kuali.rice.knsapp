fc8302c74fe65b647a6ad5c862b76f80837ef2a8
cniesen <claus-github@niesens.com>
2013-08-15 15:39:54 -0500

    Added Book lookup with column totaling

---
+++ knsapp/src/main/java/org/kuali/rice/knsapp/Book.java
@@ -0,0 +1,70 @@
+package org.kuali.rice.knsapp;
+
+import java.util.Date;
+
+import org.kuali.rice.core.api.util.type.KualiDecimal;
+import org.kuali.rice.core.api.util.type.KualiInteger;
+import org.kuali.rice.krad.bo.PersistableBusinessObjectBase;
+
+public class Book extends PersistableBusinessObjectBase {
+	private static final long serialVersionUID = -2406918292069639330L;
+
+	private Long id;
+	private String title;
+	private String typeCode;
+	private String isbn;
+	private String publisher;
+	private Date publicationDate;
+	private KualiDecimal price;
+	private KualiInteger rating;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTypeCode() {
+		return typeCode;
+	}
+	public void setTypeCode(String typeCode) {
+		this.typeCode = typeCode;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+	public KualiDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(KualiDecimal price) {
+		this.price = price;
+	}
+	public KualiInteger getRating() {
+		return rating;
+	}
+	public void setRating(KualiInteger rating) {
+		this.rating = rating;
+	}
+	
+}
